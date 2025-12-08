b4e6541eae67ce9c61eecf585918618c0076e519
GM <gm.malik.cs@gmail.com>
2025-12-08 23:43:30 +0500

    Patterns Complete

--- src/patterns/Advance_Patterns.java
+++ src/patterns/Advance_Patterns.java
@@ -40,11 +40,60 @@ public static void Pattern(int n){
         }
         System.out.println();
 }}
+    public static void numberPyramid(int n){
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print(+i);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    }
+    public static void palindromicPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+          for(int j=i;j>=1;j--){
+             System.out.print(j);
+            }
+          for(int j=2;j<=i;j++){
+              System.out.print(j);
+          }
+            System.out.println();
+        }
+    }
+    public static void DiamondPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }}
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
         System.out.println("Enter rows no : ");
         int n=input.nextInt();
         //butterflyPattern(n);
-        Pattern(n);
+       // Pattern(n);
+       // numberPyramid(n);
+        //palindromicPattern(n);
+        DiamondPattern(n);
     }
 }
